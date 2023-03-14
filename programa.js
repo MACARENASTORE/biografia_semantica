d722cd988468e341e7e8804cd9f11744deace7f4
diego barrreto <oclusiva9204@gmail.com>
2023-03-14 08:18:05 -0500

    commit4

---
+++ programa.js
@@ -0,0 +1,9 @@
+function mostrarMensaje(){
+    console.log('hicieron click en el boton'+ new Date().toLocaleDateString)
+}
+
+//Agregar un eventListener a otroBoton
+
+let boton=document.getElementById("otroBoton");
+
+boton.addEventListener("click", ()=>{});
